19db34785f68f06f6b9e273e919367a22fd7adf6
jamesjoel <jamesjoelj@gmail.com>
2023-05-25 13:56:35 +0530

    method of array

---
+++ nodejs/ref1.js
@@ -0,0 +1,10 @@
+var data1 = ["blue", "red", "green", "pink"];
+
+var data2 = data1;
+
+
+data1[0]="black";
+data1[3] = "white";
+
+console.log(data1)
+console.log(data2);
---
+++ nodejs/ref2.js
@@ -0,0 +1,9 @@
+var obj1 = { name : "rohit", age : 25, city :"indore" };
+
+var obj2 = obj1;
+
+obj1.city = "mumbai";
+
+console.log(obj1)
+console.log(obj2)
+
