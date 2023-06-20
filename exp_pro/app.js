de2d07f986d6d47a155ba4f28bd4a442987203f8
jamesjoel <jamesjoelj@gmail.com>
2023-06-20 13:54:00 +0530

    form in nodejs

--- exp_pro/app.js
+++ exp_pro/app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 
 app.use(express.static(__dirname+"/assets"));
+
+app.use(express.json())
+app.use(express.urlencoded({ extended : true }));
+
 app.set("view engine", "ejs");
 
 app.get("/", (req, res)=>{
@@ -17,6 +21,16 @@ app.get("/about/info", (req, res)=>{
 app.get("/contact", (req, res)=>{
     res.render("pages/contact");
 })
+
+app.get("/addstudent", (req, res)=>{
+    res.render("pages/add_student");
+})
+
+
+app.post("/save", (req, res)=>{
+    console.log(req.body);
+})
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, ()=>{
